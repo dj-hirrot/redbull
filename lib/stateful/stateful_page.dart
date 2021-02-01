33cad8abf354d219946ef51be40a0ad815912c23
dj-hirrot <hirotoshibutani@gmail.com>
2021-02-02 02:54:56 +0900

    update: create clock

--- lib/stateful/stateful_page.dart
+++ lib/stateful/stateful_page.dart
@@ -1,8 +1,52 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class PageStateful extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Map<String, dynamic> args = ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(args["title"])
+      ),
+      body: Center(
+        child: Clock(),
+      ),
+    );
+  }
+}
+
+class Clock extends StatefulWidget {
+  @override
+  _Clock createState() => _Clock();
+}
+
+class _Clock extends State<Clock> {
+  Timer _timer;
+  final _formatter = DateFormat('HH:mm:ss');
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(
+        Duration(seconds: 1),
+            (_t) => setState(() {})
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '${_formatter.format(DateTime.now())}',
+      style: TextStyle(fontSize: 18.0),
+    );
   }
 }
